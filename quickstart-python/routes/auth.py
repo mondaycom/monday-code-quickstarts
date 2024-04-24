93454445aab97d9f6c4e93621fdf638e1312cd66
Ori Lael <orila@monday.com>
2024-04-24 10:30:19 +0300

    Add log out & delete account routes

--- quickstart-python/routes/auth.py
+++ quickstart-python/routes/auth.py
@@ -3,7 +3,7 @@ from flask import make_response, request, jsonify, Blueprint
 
 from decorators import validate_credentials_existence
 from handlers import mcode_configuration
-from services import create_jwt_token, SecureStorage
+from services import create_jwt_token, SecureStorage, validate_jwt_token
 
 auth_bp = Blueprint('auth', __name__)
 
@@ -34,7 +34,36 @@ def signup():
     """
     data = request.get_json()
     username, password = data.get('username'), data.get('password')
-    hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    storage = SecureStorage(mcode_configuration)
 
-    SecureStorage(mcode_configuration).put(username, hashed_password.decode('utf-8'))
+    if storage.get(username):
+        return 'User already exists', 409
+
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    storage.put(username, hashed_password.decode('utf-8'))
     return 'User created', 201
+
+
+@auth_bp.route('/log_out', methods=['POST'])
+def log_out():
+    """
+    Route that receives a POST request to log out the user.
+    """
+    response = make_response(jsonify({'message': 'Logged out'}), 200)
+    response.set_cookie('token', '', httponly=True, samesite='Strict')
+    return response
+
+
+@auth_bp.route('/delete_account', methods=['POST'])
+def delete_account():
+    """
+    Route that receives a POST request to delete the user account.
+    """
+    token = request.cookies.get('token')
+    username = validate_jwt_token(token).get('user')
+
+    SecureStorage(mcode_configuration).delete(username)
+
+    response = make_response(jsonify({'message': 'Logged out'}), 200)
+    response.set_cookie('token', '', httponly=True, samesite='Strict')
+    return response
