609bb280c5fb4de90823c6159c037c66c485cfc2
Ori Lael <orila@monday.com>
2024-05-02 08:24:41 +0300

    Use state correctly to prevent attacks

--- quickstart-python/routes/auth.py
+++ quickstart-python/routes/auth.py
@@ -1,8 +1,10 @@
+import secrets
 from urllib.parse import urlencode
 
-from flask import request, Blueprint, redirect
+from flask import request, Blueprint, redirect, make_response
 
 from consts import SecretKeys
+from errors import GenericBadRequestError
 from middlewares import monday_request_auth
 from services import SecureStorage, SecretService, JWTService
 
@@ -25,9 +27,18 @@ def authorize():
 
     SecureStorage.put(user_id, {'back_to_url': back_to_url})
 
-    params = {'client_id': SecretService.get_secret(SecretKeys.MONDAY_OAUTH_CLIENT_ID), 'state': user_id}
+    # The state parameter is used for CSRF protection. It's a random string sent to the server and returned back.
+    # The client should only trust the response if the returned state matches the sent state.
+    state = secrets.token_urlsafe(16)  # Generate a random state for CSRF protection
+
+    params = {'client_id': SecretService.get_secret(SecretKeys.MONDAY_OAUTH_CLIENT_ID), 'state': state}
     redirect_url = f"{SecretService.get_secret(SecretKeys.MONDAY_OAUTH_BASE_PATH)}?{urlencode(params)}"
-    return redirect(redirect_url)
+
+    response = make_response(redirect(redirect_url))
+    response.set_cookie('state', state)
+    response.set_cookie('user_id', user_id)
+
+    return response
 
 
 @auth_bp.route('/monday/callback', methods=['GET'])
@@ -37,10 +48,22 @@ def monday_callback():
     Saves the monday.com token in the storage for later access.
     """
     code = request.args.get('code')
-    user_id = request.args.get('state')
+    user_id = request.cookies.get('user_id')
+
+    validate_state()
 
     monday_token = JWTService.get_monday_token(code)
 
     back_to_url = SecureStorage.get(user_id).get('back_to_url')
     SecureStorage.put(user_id, {'monday_token': monday_token})
     return redirect(back_to_url)
+
+
+def validate_state() -> None:
+    """
+    Validates the state parameter from the OAuth2 callback.
+    """
+    returned_state = request.args.get('state')
+    saved_state = request.cookies.get('state')
+    if returned_state != saved_state:
+        raise GenericBadRequestError('Invalid state')
