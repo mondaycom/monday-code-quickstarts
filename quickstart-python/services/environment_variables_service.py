12aabd30080cf4123900bc2941f5b4894d997b4d
Ori Lael <orila@monday.com>
2024-06-23 14:00:27 +0300

    Parse the env response

--- quickstart-python/services/environment_variables_service.py
+++ quickstart-python/services/environment_variables_service.py
@@ -8,4 +8,6 @@ class EnvironmentVariablesService:
     @staticmethod
     @with_monday_api(APITypes.ENVIRONMENT_VARIABLES, "get_environment_variable")
     def get_environment_variable(key: str, api_instance: EnvironmentVariablesApi = None):
-        return api_instance.get_environment_variable(key)
+        response = api_instance.get_environment_variable(key)
+        return response.to_dict() if response else None
+
