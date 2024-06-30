f13a271a296663dd45e39ae5479155ee2324d79c
Ori Lael <orila@monday.com>
2024-06-30 19:21:21 +0300

    Align data contracts to new JsonDataContract

--- quickstart-python/main.py
+++ quickstart-python/main.py
@@ -6,11 +6,14 @@ from werkzeug.exceptions import HTTPException
 from errors import handle_internal_error, handle_general_http_exception, handle_base_error, BaseError, \
     InternalServerError
 from routes import worker_queue_bp, mail_bp, auth_bp
+from routes.developing import developing_bp
 
 app = Flask(__name__)
 app.register_blueprint(worker_queue_bp)
 app.register_blueprint(mail_bp, url_prefix="/mail")
 app.register_blueprint(auth_bp, url_prefix="/auth")
+
+app.register_blueprint(developing_bp, url_prefix="/dev")
 
 app.register_error_handler(HTTPException, handle_general_http_exception)
 app.register_error_handler(InternalServerError, handle_internal_error)
