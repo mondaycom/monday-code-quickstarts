6cb632786a15a9853bc074bdc9979b8b566aaf45
Ori  Lael <oricho123@gmail.com>
2024-11-24 10:34:41 +0200

    Use the new logger payload option (#28)
    * Use the new logger payload option
    
    * Add health route

--- quickstart-python/services/mail_service.py
+++ quickstart-python/services/mail_service.py
@@ -8,5 +8,6 @@ class MailService:
         Function that sends an email to the given address with the given content.
         """
         # Send email logic
-        LogsService.info(f"Sending mail to {mail_address} with content: {content}")
+        LogsService.info(f"Sending mail to {mail_address} with content: {content}",
+                         payload={'content': content, 'mail_address': mail_address})
         pass
